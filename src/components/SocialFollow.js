bd4e77015cfa942a28caf98ab1ab2ae0560a6920
jerrychen-2018 <chen3294@purdue.edu>
2020-12-15 23:26:04 -0800

    reformatted navbar with designs

---
+++ src/components/SocialFollow.js
@@ -0,0 +1,28 @@
+/*import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedIn,
+  faFacebook,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialFollow() {
+  return (
+    <div class="social-container">
+      <h3>Social Follow</h3>
+      <a href="https://www.linkedin.com/in/jerry-chen2018/"
+        className="linkedin social">
+        <FontAwesomeIcon icon={faLinkedIn} size="2x" />
+      </a>
+      <a href="https://www.facebook.com/jerrychen2000/"
+        className="facebook social">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/jerrychen_2000/"
+        className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+    </div>
+  );
+}
+*/
